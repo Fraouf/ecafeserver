299a02a6e1514cfcea716414e926a35114cb2c67
Guillaume Viguier <guillaume@wedus.org>
2009-10-22 13:37:47 -0400

    Removed unnecessary files and added copyright information

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,3 +1,22 @@
+# Copyright (C) 2009 Guillaume Viguier-Just
+#
+# Author: Guillaume Viguier-Just <guillaume@viguierjust.com>
+#
+# This file is part of ecafeserver.
+#
+# Ecafeserver is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ecafeserver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ecafeserver.  If not, see <http://www.gnu.org/licenses/>.
+
 class ClientsController < ApplicationController
 
   layout nil
@@ -166,7 +185,7 @@ class ClientsController < ApplicationController
       if @client.type() == "timecode"
         Operation.add(operation_type, "operations.timecode", operation_action, @client.timecode.code)
       elsif @client.type() == "customer"
-        Operation.add(operation_type, "operations.customer", operation_action, @client.customer.login)
+        Operation.add(operation_type, "operations.customer", operation_action, @client.customer.uid)
       end
       if @client.state == "connected"
         @client.disconnect!
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,3 +1,22 @@
+# Copyright (C) 2009 Guillaume Viguier-Just
+#
+# Author: Guillaume Viguier-Just <guillaume@viguierjust.com>
+#
+# This file is part of ecafeserver.
+#
+# Ecafeserver is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ecafeserver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ecafeserver.  If not, see <http://www.gnu.org/licenses/>.
+
 class EmployeesController < ApplicationController
 	before_filter :admin_required, :except => [:edit_profile, :update_profile]
 	
